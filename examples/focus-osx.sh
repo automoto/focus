3129f4726ea0ec71562d0d18b6d9aadac60bea7d
gcase555 <g@devnull.com>
2024-03-05 18:06:02 -0600

    adding focus script for OSX

--- examples/focus-osx.sh
+++ examples/focus-osx.sh
@@ -13,14 +13,16 @@ while [ true ] ; do
   echo "\n## $opt_focus min time block ##\n"
   # blink1-tool -l 0 --red | exit
   osascript -e 'display notification "block beginning" with title "Starting Focused Work Block"'
+  ./spt play --name "Ambient '24" --playlist
   curl -X POST "https://www.rescuetime.com/anapi/start_focustime?key=$API_KEY&duration=$opt_focus"
-  killall Slack | cat
+  killall Slack
   sleep $focus_time
 
   # break
   echo "\n## $opt_rest min break ##\n"
   # blink1-tool -l 0 --green | exit
   osascript -e 'display notification "breaktime" with title "Break for Time Block"'
+  ./spt playback --toggle
   curl -X POST "https://www.rescuetime.com/anapi/end_focustime?key=$API_KEY"
   open -a Slack -j
   sleep $rest_time
--- focus.sh
+++ focus.sh
@@ -13,6 +13,7 @@ while [ true ] ; do
   echo "\n## $opt_focus min time block ##\n"
   # blink1-tool -l 0 --red | exit
   osascript -e 'display notification "block beginning" with title "Starting Focused Work Block"'
+  ./spt play --name "Ambient '24" --playlist
   curl -X POST "https://www.rescuetime.com/anapi/start_focustime?key=$API_KEY&duration=$opt_focus"
   killall Slack
   sleep $focus_time
@@ -21,6 +22,7 @@ while [ true ] ; do
   echo "\n## $opt_rest min break ##\n"
   # blink1-tool -l 0 --green | exit
   osascript -e 'display notification "breaktime" with title "Break for Time Block"'
+  ./spt playback --toggle
   curl -X POST "https://www.rescuetime.com/anapi/end_focustime?key=$API_KEY"
   open -a Slack -j
   sleep $rest_time
